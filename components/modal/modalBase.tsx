cd53002e03b866f1ee102a71c18b3a8db01377b2
Elias Jose Acevedo <ejacevedo@ri.gob.do>
2022-08-10 04:26:21 +0000

    adding button for close modal

--- components/modal/modalBase.tsx
+++ components/modal/modalBase.tsx
@@ -3,13 +3,22 @@ import { ModalContext } from "../../contexts/modalContext"
 
 
 function ModalBase() {
-    const [ modalInfo ] = useContext(ModalContext)
+    const [ modalInfo, setModalInfo ] = useContext(ModalContext)
     
+    const closeModal = () => {
+        setModalInfo((pv) => {
+            const newState = {...pv}
+            newState.visible = false
+            return newState
+        })
+    }
+
     return (
         <>
-            <input type="checkbox" id="modal" className="modal-toggle" checked={modalInfo.visible}/>
+            <input onChange={()=>{}} type="checkbox" id="modal" className="modal-toggle" checked={modalInfo.visible}/>
             <label htmlFor="modal" className="modal cursor-pointer">
                 <label className="modal-box relative" htmlFor="">
+                    <button onClick={closeModal} className="btn btn-sm btn-circle absolute right-2 top-2">✕</button>
                     {modalInfo.children}
                 </label>
             </label>
--- pages/affiliates/index.tsx
+++ pages/affiliates/index.tsx
@@ -1,59 +1,67 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useContext } from 'react'
 import BasePage from '../../components/basePage'
 import CrudBase, { CrudBaseProps } from '../../components/crudBase/crudBase'
+import { ModalContext } from '../../contexts/modalContext'
 
-const crudProps: CrudBaseProps = {
-  headerProps: {
-    name: "Afiliados",
-    onCreate: () => {
 
-    }
-  },
-  hookProps: {
-    paths: {
-      create: "",
-      delete: "",
-      get: "",
-      update: ""
-    },
-    initialState: [
-      {
-        id: "1",
-        firstname: "test",
-        lastname: "ing",
-      }
-    ],
-    initialSync: false
-    
-  }, 
-  tableProps: {
-    elements: [],
-    options: [
-      {
-        id: "edit",
-        children: <button className="btn mr-1">E</button>,
-        onClick: (e) => {
-          console.log(e)
-        }
-      },
-      {
-        id: "delete",
-        children: <button className="btn bg-red-700 hover:bg-red-800">B</button>,
-        onClick: (e) => {
-          console.log(e)
-        }
-      }
-    ],
-    schema: {
-      id: "Identificador",
-      firstname: "Nombre",
-      lastname: "Apellido",
-    }
-  }
-} 
 
 const AffiliatePage: NextPage = () => {  
+  const [modalInfo, setModalInfo] = useContext(ModalContext)
+  const crudProps: CrudBaseProps = {
+    headerProps: {
+      name: "Afiliados",
+      onCreate: () => {
+        setModalInfo({
+          children: <>Testing</>,
+          visible: true
+        })
+      }
+    },
+    hookProps: {
+      paths: {
+        create: "",
+        delete: "",
+        get: "",
+        update: ""
+      },
+      initialState: [
+        {
+          id: "1",
+          firstname: "test",
+          lastname: "ing",
+        }
+      ],
+      initialSync: false
+      
+    }, 
+    tableProps: {
+      elements: [],
+      options: [
+        {
+          id: "edit",
+          children: <button className="btn mr-1">E</button>,
+          onClick: (e) => {
+            console.log(e)
+          }
+        },
+        {
+          id: "delete",
+          children: <button className="btn bg-red-700 hover:bg-red-800">B</button>,
+          onClick: (e) => {
+            console.log(e)
+          }
+        }
+      ],
+      schema: {
+        id: "Identificador",
+        firstname: "Nombre",
+        lastname: "Apellido",
+      }
+    }
+  } 
+
   return (
     <div>
       <Head>
